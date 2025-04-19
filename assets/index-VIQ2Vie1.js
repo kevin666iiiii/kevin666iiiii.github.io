47f532bcd2ee6afc3370576909c5e2073146c2b6
kevin666iiiii <jingdis@andrew.cmu.edu>
2025-04-19 00:12:56 -0700

    Update index-VIQ2Vie1.js

--- assets/index-VIQ2Vie1.js
+++ assets/index-VIQ2Vie1.js
@@ -67680,7 +67680,7 @@ function kZ() {
               ],
             }),
             b.jsxs('p', {
-              className: 'timeline-text top-[50%] right-[10%]',
+              className: 'timeline-text top-[45%] right-[10%]',
               children: ['GoValley',
                 b.jsx('br', {}),
                 b.jsxs('span', {
